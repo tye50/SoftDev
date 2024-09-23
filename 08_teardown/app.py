9e39d0cff4fb9ea49d8806dbf0e2d2932a2a59ad
Tracy Ye <tye50@stuy.edu>
2024-09-23 03:37:38 -0400

    may questions, but completed 08

--- 08_teardown/app.py
+++ 08_teardown/app.py
@@ -3,22 +3,29 @@
 Tracy Ye
 Loonch
 SoftDev
-K08 -- Learnination Via Deconstruction : Leanring about virtual environments, flask, and this app.py thingy.
+K08 -- Learnination Via Deconstruction : Learning about virtual environments, flask, and this app.py thingy.
 2024-09-20
-Time spent:
+Time spent: 25
 '''
 
 '''
 DISCO:
 <note any discoveries you made here... no matter how small!>
 
+Piazza has instructions on how to create a virtual env on windows. The screen says "This is
+a development server. Do not use it in a production deployment. Use a production WSGI server
+instead." and links to a https website. When I open it, it says "No hablo queso!", which is
+the same as typed in your hello_world() function down there. Pero hablo queso.
+
+
+
 QCC:
-0. 
-1. 
-2. 
-3. 
-4. 
-5. 
+0. Not sure, but working with websites gives me HTML ptsd.
+1. Maybe it refers to a path like in the terminal?
+2. You print to the website page? 
+3. You print what is in the function you defined.
+4. You are printing to the website? Because I opened the website link.
+5. I'm not quite sure what this is similar to. We did work with printing to websites using python for a bit in annual computer science.
  ...
 
 INVESTIGATIVE APPROACH:
