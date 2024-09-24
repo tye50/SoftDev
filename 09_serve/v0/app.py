6d301cbfc0b5ff286079a7e0b32ef52a77b2ef38
Tracy Ye <tye50@stuy.edu>
2024-09-24 04:37:51 -0400

    v2

--- 09_serve/v0/app.py
+++ 09_serve/v0/app.py
@@ -5,14 +5,18 @@
 from flask import Flask
 app = Flask(__name__)          # ...This reminds me of Java syntax
 
-@app.route("/")                # ..."print" prints into the terminal
+@app.route("/")                # ...This probably relates to where the link is stored?
 def hello_world():
-    print(__name__)            # ..."return" prints onto the website
-    return "No hablo queso!"   # ...if i make a new definition, nothing new gets printed!
+    print(__name__)            # ..."print" prints onto the terminal
+    return "No hablo queso!"   # ..."return" prints onto the website
 
 @app.route("/")   
 def whatHappensNow():
     print(__name__)
     return "Pero hablo queso!"
 
-app.run()                      # ...the website link doesn't change even if you change the contents of the website.
+app.run()                      # ...this runs the code
+
+'''
+the website link doesn't change even if you change the contents of the website.
+'''
--- 09_serve/v2/app.py
+++ 09_serve/v2/app.py
@@ -1,6 +1,6 @@
-# Clyde 'Thluffy' Sinclair
+# Tracy Ye
 # SoftDev
-# September 2024
+# September 23, 2024
 
 from flask import Flask
 app = Flask(__name__)             #create instance of class Flask
@@ -12,3 +12,13 @@ def hello_world():
     return "No hablo queso!"
 
 app.run()
+
+'''
+print prints to the terminal. I predict it will print "about to print __name__... (website link)"
+'''
+
+'''
+when I opened it in the terminal, it didn't immediately print the message.
+however, when i opened and then closed the website, and then closed crt+c
+the flask application, it printed the "about to print" message.
+'''
