6618854cdd3237e9d522859154acfd01bc91fb02
Tracy Ye <tye50@stuy.edu>
2024-09-24 04:28:49 -0400

    v1

--- 09_serve/v1/app.py
+++ 09_serve/v1/app.py
@@ -1,6 +1,6 @@
-# Clyde 'Thluffy' Sinclair
+# Tracy Ye
 # SoftDev
-# September 2024
+# September 23, 2024
 
 from flask import Flask
 app = Flask(__name__)            #create instance of class Flask
@@ -10,3 +10,7 @@ def hello_world():
     return "No hablo queso!"
 
 app.run()
+
+'''
+so the app.route('/') probably links the instance "app" to the website
+'''
