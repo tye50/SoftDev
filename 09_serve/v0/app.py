d97801da7744e509b9f4ce755e3ea6c8abfc6a51
Tracy Ye <tye50@stuy.edu>
2024-09-24 04:21:27 -0400

    v0

--- 09_serve/v0/app.py
+++ 09_serve/v0/app.py
@@ -1,13 +1,18 @@
-# Clyde 'Thluffy' Sinclair
+# Tracy Ye
 # SoftDev
-# September 2024
+# September 23, 2024
 
 from flask import Flask
-app = Flask(__name__)          # ...
+app = Flask(__name__)          # ...This reminds me of Java syntax
 
-@app.route("/")                # ...
+@app.route("/")                # ..."print" prints into the terminal
 def hello_world():
-    print(__name__)            # ...
-    return "No hablo queso!"   # ...
+    print(__name__)            # ..."return" prints onto the website
+    return "No hablo queso!"   # ...if i make a new definition, nothing new gets printed!
 
-app.run()                      # ...
+@app.route("/")   
+def whatHappensNow():
+    print(__name__)
+    return "Pero hablo queso!"
+
+app.run()                      # ...the website link doesn't change even if you change the contents of the website.
