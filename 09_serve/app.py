c00453bc750a6389fdd708ef32e8473a094abc18
Tracy Ye <tye50@stuy.edu>
2024-09-25 05:26:25 -0400

    finished 09

--- 09_serve/app.py
+++ 09_serve/app.py
@@ -24,15 +24,19 @@ percentages = percentages[1:-1] # remove the first and last
 
 def select_occupation(occupation_list, percentages):
     return random.choices(occupation_list, weights=percentages, k=1) # using the built in weighted random, return a random occupation
-     
+    
+def ret(): # return using html syntax
+    ret = "TNPG = thisNewGroup<br>Roster = Tracy?<br><br><h1>" + str(select_occupation(occupation_list, percentages)) + "</h1><br><br>"+ str(occupation_list)
+    return ret
+    
     # -------------------------------------------------------------
-
+    
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def randOccupation():
     print(__name__)
-    return select_occupation(occupation_list, percentages) # calling the function defined above
+    return ret() # calling the function defined above
 
 app.run(port=5001) # We were having trouble in class with port 5000 already being used, so we specified the port
