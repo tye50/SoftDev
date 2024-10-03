efc7e988b45790a39e30390980781c9b85748b52
Tracy Ye <tye50@stuy.edu>
2024-10-03 01:50:13 -0400

    web page down

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -11,6 +11,7 @@ import csv, random
 jobs = []
 percents = []
 header = []
+
 with open ("data/occupations.csv", newline='') as csvfile:
     file = csv.DictReader(csvfile)
     for i in file:
@@ -21,15 +22,23 @@ with open ("data/occupations.csv", newline='') as csvfile:
 jobs = jobs[:len(jobs)-1]
 percents = percents[:len(percents)-1]
 
-print(header)
+title = "Weighted Jobs in Room 250"
+roster = ["Tracy Ye", "Jessica Yu", "Stella Yampolsky"]
+
+def randWeighted():
+    return random.choices(jobs, weights = percents) # (sequence, weight, num returns)
 # -------------------------------------------------------
 
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return "<center>"+"<br>"*15+"<a href= 'http://127.0.0.1:5000/wdywtbwygp'>Click! Here!</a></center>"
+    
 @app.route("/wdywtbwygp")
 def banana():
-    return render_template("tablified.html", j = jobs, p = percents, h = header)
+    return render_template("tablified.html", j = jobs, p = percents, h = header, t = title, r = roster, randW = randWeighted())
     
 if __name__ == "__main__":
         app.debug = True
