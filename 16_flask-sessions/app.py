3d44aa31ec38e4739065439b03acc065c01ebd5d
Tracy Ye <tye50@stuy.edu>
2024-10-10 04:07:39 -0400

    k16

---
+++ 16_flask-sessions/app.py
@@ -0,0 +1,25 @@
+# Tracy Ye
+# SoftDev
+# Oct 09, 2024
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/")
+def disp_loginpage():
+    return render_template( 'login.html' )
+
+
+@app.route("/resp", methods=['GET', 'POST'])
+def response():
+    if request.method == "GET":
+        return render_template("response.html", r = request.args['username'], method = request.method) #response to a form submission
+    if request.method == "POST":
+        return render_template("response.html", r = request.form['username'], method = request.method) #response to a form submission
+    
+if __name__ == "__main__": 
+    app.debug = True 
+    app.run()
