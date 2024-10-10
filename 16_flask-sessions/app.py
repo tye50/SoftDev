f37d03b128463df5e52df56d4dc8f6d62464a93c
Tracy Ye <tye50@stuy.edu>
2024-10-10 11:47:55 -0400

    K16 halplp

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -5,20 +5,31 @@
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
+from flask import session
+import os
 
 app = Flask(__name__)    #create Flask object
+app.secret_key = os.urandom(32)
 
 @app.route("/")
 def disp_loginpage():
-    return render_template( 'login.html' )
-
+    if request.cookies.get('username') is None:
+        return render_template( 'login.html' )
+    else:
+        return render_template("response.html", r = request.cookies.get('username'), method = request.method)
 
 @app.route("/resp", methods=['GET', 'POST'])
 def response():
     if request.method == "GET":
-        return render_template("responses.html", r = request.args['username'], method = request.method) #response to a form submission
+        session['username'] = request.args['username']
+        return render_template("response.html", r = request.args['username'], method = request.method) #response to a form submission
     if request.method == "POST":
-        return render_template("responses.html", r = request.form['username'], method = request.method) #response to a form submission
+        session['username'] = request.form['username']
+        return render_template("response.html", r = request.form['username'], method = request.method) #response to a form submission
+    
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    return render_template("logout.html")
     
 if __name__ == "__main__": 
     app.debug = True 
