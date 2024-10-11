b2b851f7886b972950cc96241f3297b7ae311c1b
Tracy Ye <tye50@stuy.edu>
2024-10-11 11:46:28 -0400

    K16

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -13,22 +13,27 @@ app.secret_key = os.urandom(32)
 
 @app.route("/")
 def disp_loginpage():
-    if request.cookies.get('username') is None:
-        return render_template( 'login.html' )
+    if 'username' in session:
+        return render_template("response.html", r = session['username'], method = request.method)
     else:
-        return render_template("response.html", r = request.cookies.get('username'), method = request.method)
+        return render_template( 'login.html' )
 
 @app.route("/resp", methods=['GET', 'POST'])
 def response():
-    if request.method == "GET":
+    if 'username' in session:
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(request.method)
+        return render_template("response.html", r = session['username'], method = request.method)
+    elif request.method == "GET":
         session['username'] = request.args['username']
         return render_template("response.html", r = request.args['username'], method = request.method) #response to a form submission
-    if request.method == "POST":
+    elif request.method == "POST":
         session['username'] = request.form['username']
         return render_template("response.html", r = request.form['username'], method = request.method) #response to a form submission
     
 @app.route("/logout", methods=['GET', 'POST'])
-def logout():
+def logout(): 
+    session.pop('username')
     return render_template("logout.html")
     
 if __name__ == "__main__": 
