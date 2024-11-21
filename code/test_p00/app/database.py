d58cc831c21dd3ff97975dba9c7520f01a62fc99
Tracy Ye <tye50@stuy.edu>
2024-11-21 11:18:18 -0500

    d4

--- code/test_p00/app/database.py
+++ code/test_p00/app/database.py
@@ -6,14 +6,15 @@ db = sqlite3.connect("data.db")
 c = db.cursor()
 if (c.execute("SELECT * FROM sqlite_master WHERE type='table' AND name='accounts'").fetchall() == []):
     c.execute("CREATE TABLE accounts(username TEXT NOT NULL, password TEXT NOT NULL)")
-    c.execute("CREATE TABLE blogs(owner TEXT NOT NULL, title TEXT NOT NULL, entry_1 TEXT NOT NULL)")
+    c.execute("CREATE TABLE blogs(owner TEXT NOT NULL, title TEXT NOT NULL)")
 db.commit()
 db.close()
 
 def addAcc(uname, pw):
     db = sqlite3.connect("data.db")
     c = db.cursor()
-    c.execute(f"INSERT INTO accounts VALUES ('{ uname }', '{ pw }')")
+    cmd ='INSERT INTO accounts VALUES(?, ?)'
+    c.execute(cmd, (uname, pw))
     db.commit()
     db.close()
 
@@ -21,4 +22,9 @@ def newBlog(uname, title, firstEntry):
     db = sqlite3.connect("data.db")
     c = db.cursor()
     c.execute(f"CREATE TABLE { uname }---{ title }(entryID INTEGER, entry TEXT)")
-    c.execute(f"INSERT INTO blogs
+    cmd ='INSERT INTO blogs VALUES(?, ?)'
+    c.execute(cmd, (uname, title))
+    cmd =f'INSERT INTO { uname }---{ title } VALUES(?, ?)'
+    c.execute(cmd, ('entry_1', firstEntry))
+    c.commit()
+    c.close()
