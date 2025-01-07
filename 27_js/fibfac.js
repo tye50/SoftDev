66ef356fc986335252146be30bc0ea0cc0a6465c
Tracy Ye <tye50@stuy.edu>
2025-01-07 11:44:56 -0500

    tested K27 HTML page

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -19,14 +19,14 @@ function fact(n){
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
 
-fact(1) = 1
-fact(2) = 2
-fact(3) = 6
-fact(4) = 24
-fact(5) = 120
-fact(6) = 720
-fact(7) = 5040
-fact(8) = 40320   // yes works
+// fact(1) = 1
+// fact(2) = 2
+// fact(3) = 6
+// fact(4) = 24
+// fact(5) = 120
+// fact(6) = 720
+// fact(7) = 5040
+// fact(8) = 40320   // yes works
 
 //-----------------------------------------------------------------
 
@@ -48,14 +48,14 @@ function fib(n){
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
 
-fib(0) = 0
-fib(1) = 1
-fib(2) = 1
-fib(3) = 2
-fib(4) = 3
-fib(5) = 5
-fib(6) = 8
-fib(7) = 13
-fib(8) = 21   // yes works
+// fib(0) = 0
+// fib(1) = 1
+// fib(2) = 1
+// fib(3) = 2
+// fib(4) = 3
+// fib(5) = 5
+// fib(6) = 8
+// fib(7) = 13
+// fib(8) = 21   // yes works
 
 //=================================================================
