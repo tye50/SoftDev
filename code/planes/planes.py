cf3fafb28284d79da52bbf29673e40c4e96b9ac4
Tracy Ye <tye50@stuy.edu>
2025-03-25 05:17:50 -0400

    planez

--- code/planes/planes.py
+++ code/planes/planes.py
@@ -1,10 +1,53 @@
-import math, random
+import math, random, time
 
 def createPlane(r, c):
     return [[0]*c]*r
 
-for i in createPlane(32, 4):
-    print(i)
+def createSchedule(): # (plane, time, has_seats_available)
+    sched = []
+    for i in range(3):
+        sched.append([createPlane(32, 4), random.randint(1, 24), True])
+    sched=sorted(sched, key=lambda y: y[1])
+    return sched
 
-def createSchedule():
-    return [[createPlane(32, 4), random.randint(1, 24)]*2]
+def availableFlights(sched):
+    times=[]
+    for i in sched:
+        if i[2] == True:
+            if i[1] not in times:
+                times.append(i[1])
+    return times
+
+def suggestAlt0(times, val): # suggest closest time when not available
+    closest=1000
+    for i in times:
+        if abs(val-i)<closest or abs(i-val)<closest:
+            closest=i
+    return closest
+
+def main():
+    sched = createSchedule()
+    passengers_sat = 0
+    groups_split = 0
+    num_split = 0
+    
+    # loop should start here
+    available_times = availableFlights(sched)
+    print("We have planes departing at: "+str(available_times))
+    t = int(input("What time would you like to depart? [Select 1-24] "))
+    if t not in available_times:
+        s0 = input("That time is not available. The closest departing flight to that time is "+str(suggestAlt0(available_times, t))+". Would you like to switch over? [Select Y/N] ").lower()
+        if s0 == "y" or s0 == "yes":
+            t = suggestAlt0(available_times, t)
+        # ELSE (END + COUNT)
+
+
+    # print-pront
+    time.sleep(2)
+    for i in sched:
+        print("Departs at: "+str(i[1]))
+        print("Available seats: "+str(i[2]))
+        for j in i[0]:
+            print(j)
+        print()
+main()
